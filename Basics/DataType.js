1a77ddc3134ae8b49d9bb86a5623f084b928ed3f
Jayesh158 <sonawanejayesh947@gmail.com>
2025-07-25 12:11:19 +0530

    New file created of datatypes

---
+++ Basics/DataType.js
@@ -0,0 +1,9 @@
+let age = 20 // 20 is a number data type
+
+let name = "Jay" // "Jay" is a string data type
+
+let isEmployed = true // true is a boolean data type
+
+console.log(typeof age); // typeof operator is used to determine the type of a variable or expression
+
+// alert() is used to display alert message 
