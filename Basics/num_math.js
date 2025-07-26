5dd2e5ad50b10a66c3f02d182485d0fe5920cfaa
Jayesh158 <sonawanejayesh947@gmail.com>
2025-07-26 15:43:57 +0530

    Learn maths in js

--- Basics/num_math.js
+++ Basics/num_math.js
@@ -1,13 +1,36 @@
-let score = 400
-console.log(score);
+const score = 400
+//console.log(score);
+/*
+const balance = new Number(100)
+console.log(balance); 
 
-let score2 = "500"
+console.log(balance.toString().length); // Convert to string and get length
 
-let value = new Number(200)
-console.log(value)
+console.log(balance.toFixed(1)); // Convert to string with 1 decimal place
 
-let decimalNumber = 158.2020
+const otherNumber = 123.8966
 
-let BigValue = 1000000000
+console.log(otherNumber.toPrecision(4)); // Convert to string with 4 significant digits
+  
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN')); // Convert to string with locale formatting
 
-console.log(score.toString().length)
+*/
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4)); // Absolute value
+console.log(Math.sqrt(121)); // Square root
+console.log(Math.round(4.6)); // Round to nearest integer
+console.log(Math.pow(2, 6)); // Power (2 raised to the power of
+console.log(Math.ceil(4.2)); // Round up to nearest integer
+console.log(Math.floor(4.9)); // Round down to nearest integer
+console.log(Math.min(4, 3, 6, 8)); // Find minimum value
+console.log(Math.max(4, 3, 6, 8)); // Find maximum value
+
+console.log(Math.random()); // Generate a random number between 0 and 1
+
+console.log(Math.floor((Math.random(1)*6) + 1)); 
+
+
