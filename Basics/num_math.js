ece2d02f6b20094ba3e3b3de623366070e845bda
Jayesh158 <sonawanejayesh947@gmail.com>
2025-07-26 15:28:03 +0530

    all about numbers

---
+++ Basics/num_math.js
@@ -0,0 +1,13 @@
+let score = 400
+console.log(score);
+
+let score2 = "500"
+
+let value = new Number(200)
+console.log(value)
+
+let decimalNumber = 158.2020
+
+let BigValue = 1000000000
+
+console.log(score.toString().length)
