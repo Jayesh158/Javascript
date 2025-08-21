335f91867bed5a2595386b22839a7a002a08215a
Jayesh158 <sonawanejayesh947@gmail.com>
2025-08-21 14:09:28 +0530

    Date in javascript

--- Basics/Date.js
+++ Basics/Date.js
@@ -1,2 +1,19 @@
 let myDate = new Date();
+
 console.log(myDate);    
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.getDate()); 
+console.log(myDate.getDay());
+console.log(myDate.getFullYear());
+console.log(myDate.getHours());
+console.log(myDate.getMinutes());
+console.log(myDate.getSeconds()); 
+
+
+let myBirthDate = new Date(2004 , 1 , 15 )
+console.log(`I was born on ${ myBirthDate.toDateString() }`);
+
