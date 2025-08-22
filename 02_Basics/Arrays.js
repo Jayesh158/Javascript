dfda6c147627eadc1d2bf1c563827dfaee6c39cc
Jayesh158 <sonawanejayesh947@gmail.com>
2025-08-22 14:07:49 +0530

    Array in javascript

---
+++ 02_Basics/Arrays.js
@@ -0,0 +1,14 @@
+// push , pop , shift , unshift , splice , slice , indexOf , includes , reverse , concat , join
+
+const cities = ["Nashik , Mumbai , Pune , Chennai , Banglore , Jaipur "]
+
+const states = ["Maharashtra" ,"Punjab" , "Gujrat" , "Kashmir" , "Haryana" , "Rajasthan"]
+
+cities.push("Mohali") // add element at last
+console.log(cities)
+cities.pop() // remove element from last
+
+states.unshift("Hyderabad") // add element at first
+console.log(states)
+states.shift() // remove element from first 
+console.log(states)
